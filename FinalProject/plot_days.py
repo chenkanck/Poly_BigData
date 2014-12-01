72129fbd01a6c1907ac799e37be221bd1ad3d466
chenkanck <chemkanck1990@gmail.com>
2014-11-30 23:24:23 -0500

    Figure daily trips
    Draw a figure to show the daily trips.
    First argument: input
    Second argument: output

---
+++ FinalProject/plot_days.py
@@ -0,0 +1,19 @@
+import matplotlib, sys
+matplotlib.use('Agg')
+import matplotlib.pyplot as m_plot
+
+if __name__=='__main__':
+    counts = []
+    with open(sys.argv[1], 'r') as f:
+        for line in f:
+            counts.append(map(int, line.strip().split('\t')))
+    counts.sort()
+    for a in counts:
+        print a
+    fig = m_plot.figure(figsize=(11, 4))
+    fig.suptitle('Trips vs Time', fontsize=20)
+    ax = fig.add_subplot(111)
+    # ax.plot_date(*zip(*counts), fmt='r-', aa=True)
+    ax.plot(*zip(*counts))
+    ax.set_ylim([0, 600000])
+    fig.savefig(sys.argv[2])
