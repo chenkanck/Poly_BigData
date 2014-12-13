aa3a10dc5a4564d14d2e68abc6cbfc642fcef7be
chenkanck <chemkanck1990@gmail.com>
2014-12-13 14:58:24 -0500

    Add OnedayTrips folder

---
+++ FinalProject/OneDayTrips/TripsByInterval_daily_plot.py
@@ -0,0 +1,27 @@
+import matplotlib, sys
+matplotlib.use('Agg')
+import matplotlib.pyplot as m_plot
+def parseTime(strtime):
+    h_m = strtime.split(':')
+    mins = (float(h_m[1]))/60
+    mins = float ('%.2f' %mins)
+    return int(h_m[0])+mins
+
+if __name__=='__main__':
+    counts = []
+    with open(sys.argv[1], 'r') as f:
+        for line in f:
+            pair = line.strip().split('\t')
+            pair[0] = parseTime(pair[0])
+            
+            counts.append(map(float, pair))
+    counts.sort()
+    for a in counts:
+        print a
+    fig = m_plot.figure(figsize=(11, 4))
+    fig.suptitle('Trips vs Time Aug 11', fontsize=20)
+    ax = fig.add_subplot(111, xticks = [2,4,6,8,10,12,14,16,18,20,22,24])
+    # ax.plot_date(*zip(*counts), fmt='r-', aa=True)
+    ax.plot(*zip(*counts))
+    ax.set_ylim([0, 5000])
+    fig.savefig(sys.argv[2])
---
+++ FinalProject/OneDayTrips/mapper_aug4.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+import sys, time
+#works on interval-src-dst (intermediate data)
+#output the intervals in exact one day
+
+# input comes from STDIN (standard input)
+for line in sys.stdin:
+    # remove leading and trailing whitespace
+    line = line.strip()
+    # split the line into words
+    datas = line.split('\t')
+    # data[0] interval,src-dst
+    # data[1] numbers
+    if len(datas)>1:
+        try :
+            part = datas[0].split(',')
+            interval = part[0]
+            day_st_to_end = interval.split(' ')
+            date = time.strptime(day_st_to_end[0],'%Y-%m-%d')
+            if date.tm_mon != 8 or date.tm_mday!=4:
+                continue
+            # output_key
+            print '%s\t%s' % (day_st_to_end[1], datas[1])
+        except IndexError:
+            pass
